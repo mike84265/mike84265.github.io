88266cd8e23e9ceee4a0da85c3c7c9e9c8d2dd44
mike84265 <b03901078@ntu.edu.tw>
2023-09-24 20:09:26 +0800

    Removed unnecessary files and minor layout update

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # bundle exec jekyll serve --port 8888  --livereload --incremental
-bundle exec jekyll serve --port 8888 
+# bundle exec jekyll serve --port 8888
+export JEKYLL_ENV=production
+bundle exec jekyll serve --host 0.0.0.0 --port=8888 
